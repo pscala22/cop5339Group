e37be3033c774111c56ba0162fc17adfaa169755
Martinraj-1998 <mnadar2022@fau.edu>
2023-04-30 18:16:22 -0400

    Added the Product class

---
+++ TestProject/src/com/test/Product.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import java.util.Objects;
+
+public class Product {
+	private final String name;
+	private final double price;
+
+	public Product(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+
+}
