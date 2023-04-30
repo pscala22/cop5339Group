0521d46de37fc934a1aba6882ee8dc3bdd2053f3
Martinraj-1998 <mnadar2022@fau.edu>
2023-04-30 18:32:06 -0400

    Changed toString

--- TestProject/src/com/test/Product.java
+++ TestProject/src/com/test/Product.java
@@ -40,7 +40,7 @@ public class Product {
 
 	@Override
 	public String toString() {
-		return "Product [name=" + name + ", price=" + price + "]";
+		return "Product: [name=" + name + ", price=" + price + "]";
 	}
 
 }
